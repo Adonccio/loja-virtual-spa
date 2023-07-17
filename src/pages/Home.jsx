9ba484d725da5628502cdc89ee10ca9dcf81c04b
GUSTAVO ADONCIO <gustavoadoncio@gmail.com>
2023-07-17 12:46:54 -0300

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Header from "../Header";
+
+export default function Home () {
+    return (
+    <>
+    
+    <Header/>
+        <h1>Bem-Vindo!</h1>
+        <p>Faça já sua compra.</p>
+    </>)
+}
